fbdc5c59ecd4a60f7e74b7edb49261a5b86f9159
pranuashdeepu <praneethreddynallapareddy2588@gmail.com>
2025-07-23 10:23:42 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,8 +11,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name agoproject.k8s.local --zones ap-south-1a --image ami-0f918f7e67a3323f0  --control-plane-count=1 --control-plane-size t2.large --node-count=3 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3api create-bucket --bucket raja5637dogeecloudanddevopsbyraham0073456.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket raja5637dogeecloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://raja5637dogeecloudanddevopsbyraham0073456.k8s.local
+kops create cluster --name pranu.k8s.local --zones us-east-1a --image ami-05ffe3c48a9991133  --control-plane-count=1 --control-plane-size t2.large --node-count=2 --node-size t2.medium
+kops update cluster --name pranu.k8s.local --yes --admin
